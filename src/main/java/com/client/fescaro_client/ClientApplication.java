0b3cd24d4f0226385f0e36333361dacb49c66cbc
happymink <minkyun@kakao.com>
2024-04-09 13:40:37 +0900

    commit

--- src/main/java/com/client/fescaro_client/ClientApplication.java
+++ src/main/java/com/client/fescaro_client/ClientApplication.java
@@ -1,5 +1,6 @@
 package com.client.fescaro_client;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -13,6 +14,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,39 +24,35 @@ import java.nio.charset.StandardCharsets;
 
 public class ClientApplication extends Application implements EventHandler<ActionEvent> {
 
-    Socket socket;
-    TextArea textArea;
-    int port = 9876;
-    Button connectionButton;
-    Button sendButton;
-    TextField IPText;
-    TextField portText;
-    TextField userName;
-    TextField input;
+    private Socket socket;
+    private TextArea textArea;
+    private int port = 3000;
+    private Button connectionButton;
+    private Button sendButton;
+    private TextField IPText;
+    private TextField portText;
+    private TextField userName;
+    private TextField input;
+    private PauseTransition pause = new PauseTransition(Duration.millis(350));
 
     public static void main(String[] args) {
         launch();
     }
 
-    //클라이언트 프로그램 동작 메서드
-    public void startClient(String IP, int port) {
+    private void startClient(String IP, int port) {
         Thread thread = new Thread(() -> {
             try {
-                System.out.println("소켓 연결을 시도합니다.");
                 socket = new Socket(IP, port);
-                System.out.println(socket);
                 receive();
             } catch (Exception e) {
                 if (socket == null){
                     stopClient();
-                    Platform.runLater(() ->
-                            textArea.appendText("[서버 연결 실패]\n"));
+                    Platform.runLater(() -> textArea.appendText("[서버 연결 실패]\n"));
                     connectionButton.setText("접속하기");
                     input.setDisable(true);
                     sendButton.setDisable(true);
-
                 } else if (!socket.isConnected()) {
-                    System.out.println("[서버 접속 실패]");
+                    Platform.runLater(() -> textArea.appendText("[서버 연결 실패]\n"));
                     stopClient();
                     Platform.exit();
                 }
@@ -63,11 +61,9 @@ public class ClientApplication extends Application implements EventHandler<Actio
         thread.start();
     }
 
-    //클라이언트 프로그램 종료 메서드
-    public void stopClient() {
+    private void stopClient() {
         try {
             if (socket != null && !socket.isClosed()) {
-                System.out.println("클라이언트에서 접속을 종료합니다.");
                 OutputStream out = socket.getOutputStream();
                 byte[] buffer = ("exit").getBytes(StandardCharsets.UTF_8);
                 out.write(buffer);
@@ -79,8 +75,19 @@ public class ClientApplication extends Application implements EventHandler<Actio
         }
     }
 
-    // 서버로부터 메세지를 전달받는 메서드
-    public void receive() {
+    private void checkCloseMessage(String message) throws IOException {
+        if(message.equals("server close")){
+            Platform.runLater(() -> textArea.appendText("서버가 중지됩니다.\n"));
+            Platform.runLater(() ->connectionButton.setText("접속하기"));
+            Platform.runLater(() -> textArea.appendText("[채팅방 퇴장]\n"));
+            stopClient();
+            socket.close();
+            input.setDisable(true);
+            sendButton.setDisable(true);
+        }
+    }
+
+    private void receive() {
         while (true) {
             try {
                 InputStream in = socket.getInputStream();
@@ -89,16 +96,7 @@ public class ClientApplication extends Application implements EventHandler<Actio
                 if (length == -1) throw new IOException();
                 String message = new String(buffer, 0, length, StandardCharsets.UTF_8);
 
-                if(message.equals("server close")){
-                    Platform.runLater(() -> textArea.appendText("서버가 중지됩니다."));
-                    connectionButton.setText("접속하기");
-                    stopClient();
-                    Platform.runLater(() ->
-                            textArea.appendText("[채팅방 퇴장]\n"));
-                    input.setDisable(true);
-                    sendButton.setDisable(true);
-                }
-
+                checkCloseMessage(message);
                 Platform.runLater(() -> textArea.appendText(message));
 
             } catch (Exception e) {
@@ -108,11 +106,9 @@ public class ClientApplication extends Application implements EventHandler<Actio
         }
     }
 
-    //서버로부터 메세지를 전송하는 메서드
-    public void send(String message) {
+    private void send(String message) {
         Thread thread = new Thread(() -> {
             try {
-                System.out.println("서버로 메세지를 전송합니다 : "+message);
                 OutputStream out = socket.getOutputStream();
                 byte[] buffer = message.getBytes(StandardCharsets.UTF_8);
                 out.write(buffer);
@@ -133,12 +129,12 @@ public class ClientApplication extends Application implements EventHandler<Actio
         hBox.setSpacing(10);
 
         userName = new TextField();
-        userName.setPrefWidth(150);
+        userName.setPrefWidth(100);
         userName.setPromptText("이름을 입력하세요");
         HBox.setHgrow(userName, Priority.ALWAYS);
 
         IPText = new TextField("127.0.0.1");
-        portText = new TextField("9876");
+        portText = new TextField("3000");
         portText.setPrefWidth(80);
 
         hBox.getChildren().addAll(userName, IPText, portText);
@@ -169,7 +165,7 @@ public class ClientApplication extends Application implements EventHandler<Actio
 
         root.setBottom(pane);
         Scene scene = new Scene(root, 800, 400);
-        stage.setTitle("[채팅 클라이언트]");
+        stage.setTitle("GUI ChatClient");
         stage.setOnCloseRequest(windowEvent -> stopClient());
         stage.setScene(scene);
 
@@ -178,41 +174,52 @@ public class ClientApplication extends Application implements EventHandler<Actio
         connectionButton.requestFocus();
     }
 
+    private void setBtnAfterBtnAction(Button button, String message){
+        button.setDisable(true);
+        pause.setOnFinished(action -> button.setDisable(false));
+        pause.play();
+        button.setText(message);
+
+        if(message.equals("종료하기")){
+            input.setDisable(false);
+            sendButton.setDisable(false);
+            input.requestFocus();
+        }
+        if(message.equals("접속하기")){
+            input.setDisable(true);
+            sendButton.setDisable(true);
+        }
+    }
+
     @Override
     public void handle(ActionEvent actionEvent) {
+
         if (actionEvent.getSource() == connectionButton) {
             if (connectionButton.getText().equals("접속하기")) {
+
+                setBtnAfterBtnAction(connectionButton, "종료하기");
                 try {
                     port = Integer.parseInt(portText.getText());
                 } catch (Exception e) {
-                    System.out.println("접속 실패");
                     e.printStackTrace();
                 }
                 startClient(IPText.getText(), port);
-                Platform.runLater(() ->
-                        textArea.appendText("[채팅방 접속]\n"));
-                connectionButton.setText("종료하기");
-                input.setDisable(false);
-                sendButton.setDisable(false);
-                input.requestFocus();
+                Platform.runLater(() -> textArea.appendText("[채팅방 접속]\n"));
             } else {
+                setBtnAfterBtnAction(connectionButton, "접속하기");
                 stopClient();
-                Platform.runLater(() ->
-                        textArea.appendText("[채팅방 퇴장]\n"));
-                connectionButton.setText("접속하기");
-                input.setDisable(true);
-                sendButton.setDisable(true);
+                Platform.runLater(() -> textArea.appendText("[채팅방 퇴장]\n"));
             }
         }
 
         if (actionEvent.getSource() == sendButton) {
-            send(userName.getText() + ": " + input.getText() + "\n");
+            send("["+userName.getText() + "] : " + input.getText() + "\n");
             input.setText("");
             input.requestFocus();
         }
 
         if (actionEvent.getSource() == input) {
-            send(userName.getText() + ": " + input.getText() + "\n");
+            send("["+userName.getText() + "] : " + input.getText() + "\n");
             input.setText("");
             input.requestFocus();
         }
